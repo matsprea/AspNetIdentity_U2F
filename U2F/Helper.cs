87a27414d6a50c65d511b8c7f0f18439d2756cd6
Matteo Spreafico <matteo.spreafico@gmail.com>
2014-11-02 18:46:15 +0100

    TrimEnd

--- U2F/Helper.cs
+++ U2F/Helper.cs
@@ -28,7 +28,7 @@ namespace U2F
 		public static string Base64Urlencode(this byte[] arg)
 		{
 			var s = Convert.ToBase64String(arg); // Regular base64 encoder
-			s = s.Split('=')[0]; // Remove any trailing '='s
+			s = s.TrimEnd('='); // Remove any trailing '='s
 			s = s.Replace('+', '-'); // 62nd char of encoding
 			s = s.Replace('/', '_'); // 63rd char of encoding
 			return s;
